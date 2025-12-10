8a037ce7332d76acae11bc6fa17920137d6c6c33
korvo <128074105+corvid3@users.noreply.github.com>
2025-12-10 15:36:50 -0500

    init commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+# tenc32 runner
+Simple project that implements a basic IO framework around the tenc32 CPU. Loads a 16k binary ROM into memory & runs the computer until power-down.
---
+++ csrc/main.c
@@ -0,0 +1,123 @@
+#include <assert.h>
+#include <bits/time.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <crow.10c32_serial/serial.h>
+#include <crow.crowcpu/crowcpu.h>
+#include <stdlib.h>
+
+#include "crow.10c32_floppy/floppy.h"
+#include "crow.crowcpu_arch/crowcpu_arch.h"
+
+void
+exception_callback(tenc32_motherboard_t* mobo)
+{
+  tenc32_dump_registers(mobo);
+}
+
+int
+main(int argc, char** argv)
+{
+  char* bios_path = NULL;
+  unsigned tps = 10;
+
+  char c;
+  while ((c = getopt(argc, argv, "b:t:")) != -1) {
+    switch (c) {
+      case 'b':
+        bios_path = optarg;
+        break;
+
+      case 't':
+        tps = atoi(optarg);
+        break;
+
+      case '?':
+        fprintf(stderr, "unknown option %c\n", optopt);
+        exit(1);
+        break;
+    }
+  }
+
+  unsigned interonset_msec = 1000. / tps;
+  [[maybe_unused]] unsigned interonset_nsec = 1000 * 1000 * interonset_msec;
+  [[maybe_unused]] unsigned interonset_multiplicity = 0;
+
+  // struct timespec res;
+  // clock_getres(CLOCK_MONOTONIC, &res);
+
+  // interonset_multiplicity = res.tv_nsec / interonset_nsec;
+
+  if (bios_path == NULL) {
+    fprintf(stderr, "path to bios must be provided\n");
+    exit(1);
+  }
+
+  struct stat s;
+  assert(stat(bios_path, &s) != -1);
+
+  if (s.st_size != TENC32_ROM_SIZE)
+    fprintf(stderr,
+            "provided bios file is not precisely %#x bytes\n",
+            TENC32_ROM_SIZE),
+      exit(1);
+
+  char bios[TENC32_ROM_SIZE];
+
+  {
+    FILE* bios_file = fopen(bios_path, "r");
+    [[maybe_unused]] int x = fread(bios, 1, sizeof bios, bios_file);
+    fclose(bios_file);
+  }
+
+  tenc32_configuration_t conf = {
+    .cci_con_ms = 1000,
+    .cci_update_ms = 1000,
+  };
+
+  struct tenc32_motherboard_t* mobo =
+    tenc32_motherboard_create(&conf, TENC32_MIN_RAM_SIZE);
+
+  /* initialize the serial monitor */
+  tenc32_add_io_space(mobo, tenc32_serial_create(0x10, stdout));
+
+  /* initialize the floppy disk controller peripheral
+   * we assume theres already a disk in (/dev/sdb)
+   */
+  struct cfdc* fdc = tenc32_fdc_create(mobo, 0x00);
+  tenc32_add_io_space(mobo, tenc32_fdc_create_io(fdc, 0x11));
+  if (!tenc32_fdc_insert_diskette(fdc, "/dev/sdb", 0x00))
+    return 0;
+  // tenc32_fdc_debug(fdc, STDERR_FILENO);
+
+  printf("motherboard created\n");
+  tenc32_restart(mobo, &bios);
+
+  tenc32_insert_exception_callback(mobo, exception_callback);
+
+  int state = 0;
+  [[maybe_unused]] int incrementer = 0;
+
+  do {
+    // tenc32_dump_registers(mobo);
+    // if (incrementer++ == 1) {
+    //   struct timespec ts;
+    //   ts.tv_sec = 0;
+    //   ts.tv_nsec = 20000000;
+    //   nanosleep(&ts, NULL);
+    //   incrementer = 0;
+    // }
+
+    // if (state != TENC32_STEP_OK)
+    //   fprintf(stderr, "sHITTT!!! %i\n", state);
+  } while ((state = tenc32_step(mobo)),
+           state != TENC32_STEP_POWEROFF && state != TENC32_STEP_CRASH);
+
+  tenc32_fdc_remove_diskette(fdc, 0);
+  tenc32_fdc_destroy(fdc);
+  tenc32_motherboard_destroy(mobo);
+}
